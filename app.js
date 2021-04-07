9bfbcd5e2ab42b704e022624f4e38062036f2240
Charles <cchester1@angelo.edu>
2021-04-07 08:54:20 -0500

    generate an html file

---
+++ app.js
@@ -0,0 +1,13 @@
+fs = require('fs');
+
+const generatePage = require('./src/page-template.js')
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('portfolio complete');
+})
