18b6dcb79079fcf8858cd9429dcb4b7334ba962c
Charles <cchester1@angelo.edu>
2021-04-08 19:13:01 -0500

    ..

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-fs = require('fs');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 const generatePage = require('./src/page-template.js')
 
-const profileDataArgs = process.argv.slice(2);
+const pageHTML = generatePage('name', 'github');
 
-const [name, github] = profileDataArgs;
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
 
   console.log('portfolio complete');
 })
+
+const promptUser = () => {
+inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+};
+
+promptUser().then(answers => console.log(answers));
+
